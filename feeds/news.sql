8a42bba6a85ffbdf2db9beee97f99f8fcc9d838b
Ariton84 <vanver@gmail.com>
2018-03-01 17:16:09 -0800

    sql file for our feeds

---
+++ feeds/news.sql
@@ -0,0 +1,136 @@
+#News.sql
+
+#wn18_Feeds table
+#
+DROP TABLE IF EXISTS wn18_Feeds;
+
+create table wn18_Feeds(
+FeedID int unsigned not null auto_increment primary key,
+FeedTitle varchar(120),
+FeedCategoryID int DEFAULT 0,
+FeedDescription text,
+FeedLink text,
+FeedDateTime DATETIME
+)ENGINE=INNODB;
+
+insert into wn18_Feeds(
+FeedID,
+FeedTitle,
+FeedCategoryID,
+FeedDescription,
+FeedLink,
+FeedDateTime
+)
+values(NULL, NOW());
+
+insert into wn18_Feeds(
+FeedID,
+FeedTitle,
+FeedCategoryID,
+FeedDescription,
+FeedLink,
+FeedDateTime
+)
+values(NULL, NOW());
+
+insert into wn18_Feeds(
+FeedID,
+FeedTitle,
+FeedCategoryID,
+FeedDescription,
+FeedLink,
+FeedDateTime
+)
+values(NULL, NOW());
+
+insert into wn18_Feeds(
+FeedID,
+FeedTitle,
+FeedCategoryID,
+FeedDescription,,
+FeedLink,
+FeedDateTime
+)
+values(NULL, NOW());
+
+insert into wn18_Feeds(
+FeedID,
+FeedTitle,
+FeedCategoryID,
+FeedDescription,
+FeedLink,
+FeedDateTime
+)
+values(NULL, NOW());
+
+insert into wn18_Feeds(
+FeedID,
+FeedTitle,
+FeedCategoryID,
+FeedDescription,
+FeedLink,
+FeedDateTime
+)
+values(NULL, NOW());
+
+insert into wn18_Feeds(
+FeedID,
+FeedTitle,
+FeedCategoryID,
+FeedDescription,
+FeedLink,
+FeedDateTime
+)
+values(NULL, NOW());
+
+insert into wn18_Feeds(
+FeedID,
+FeedTitle,
+FeedCategoryID,
+FeedDescription,
+FeedLink,
+FeedDateTime
+)
+values(NULL, NOW());
+
+insert into wn18_Feeds(
+FeedID,
+FeedTitle,
+FeedCategoryID,
+FeedDescription,
+FeedLink,
+FeedDateTime
+)
+values(NULL,NOW());
+
+
+#wn18_FeedCategories table
+#
+DROP TABLE IF EXISTS wn18_FeedCategories;
+
+create table wn18_FeedCategories(
+FeedCategoryID int unsigned not null auto_increment primary key,
+FeedCategoryName varchar(120),
+FeedCategoryDescription text
+)ENGINE=INNODB;
+
+insert into wn18_FeedCategories(
+FeedCategoryID,
+FeedCategoryName,
+FeedCategoryDescription
+)
+values(NULL,'Technology','Cusrrent Technology News');
+
+insert into wn18_FeedCategories(
+FeedCategoryID,
+FeedCategoryName,
+FeedCategoryDescription
+)
+values(NULL,'Science','Current Science News');
+
+insert into wn18_FeedCategories(
+FeedCategoryID,
+FeedCategoryName,
+FeedCategoryDescription
+)
+values(NULL,'Entertainment', 'Current Entertainment News');
