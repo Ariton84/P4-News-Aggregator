2916c4c91b625d3baa77628bf10687e5a237dc99
Sab2731 <t1010_2731@hotmail.com>
2018-03-04 21:12:01 -0800

    added Feeds link

--- feeds/news.sql
+++ feeds/news.sql
@@ -119,7 +119,7 @@ FeedCategoryID,
 FeedCategoryName,
 FeedCategoryDescription
 )
-values(NULL,'Technology','Cusrrent Technology News');
+values(NULL,'Technology','Cusrrent Technology News', );
 
 insert into wn18_FeedCategories(
 FeedCategoryID,
@@ -134,3 +134,49 @@ FeedCategoryName,
 FeedCategoryDescription
 )
 values(NULL,'Entertainment', 'Current Entertainment News');
+
+insert into wn18_Feeds(
+FeedID,
+FeedTitle,
+FeedCategoryID,
+FeedDescription,
+FeedLink,
+FeedDateTime
+)
+values(NULL,'Apple Park: Transcript of 911 calls about injuries from walking into glass',1,'Three 911 calls were made from Apple Park','https://www.sfchronicle.com/business/article/Apple-Park-Transcript-of-911-calls-about-12723602.php', NOW());
+
+insert into wn18_Feeds(
+FeedID,
+FeedTitle,
+FeedCategoryID,
+FeedDescription,
+FeedLink,
+FeedDateTime
+)
+values(NULL,'Galaxy S9 Vs Galaxy S8: Whats The Difference?',2,'The Galaxy S9 is here ','https://www.forbes.com/sites/gordonkelly/2018/03/04/samsung-galaxy-s9-vs-galaxy-s8-whats-the-difference/#48fc1bb87629', NOW());
+       
+       
+insert into wn18_Feeds(
+FeedID,
+FeedTitle,
+FeedCategoryID,
+FeedDescription,
+FeedLink,
+FeedDateTime
+)
+values(NULL,'YouTube tests new feature',3,'YouTube tests new feature that replaces video background images','https://mashable.com/2018/03/04/youtube-stories-green-screen/#FZ53A9DKhmqm', NOW());   
+
+
+
+
+
+
+
+
+       
+       
+       
+       
+       
+       
+       
